967d68fae5e152627e65d411190a9eedf1df6ffb
rollendxavier <42246854+rollendxavier@users.noreply.github.com>
2019-03-22 12:06:55 +0530

    Add files via upload

---
+++ HR.AzureAuthentication.HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HR.AzureAuthentication.HelloWorld.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+
+namespace HR.AzureAuthentication.HelloWorld.Controllers
+{
+    //[Authorize]
+    [Authorize(Policy = "Users")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           //// Get the group list from the Azure AD.
+          //// var groups = User.Claims.Where(c => c.Type == "groups").ToList();
+          return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
